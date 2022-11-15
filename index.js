8bb86910699a2959348dfe30230869af9e14e62c
Nicole Robbins <nicole.robbins@att.net>
2022-11-14 20:39:41 -0600

    commit tree 713ee75dc1b082b74b0eb0f4facf4f37e7872f1c

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const difference = target - array[i];
+    for (let n = i + 1; n < array.length; n++) {
+      if (array[n] === difference) return true;
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+   // iterate over the array of numbers
+  //  find the difference between the target and the current value
+  //  iterate over the rest of the numbers in the array
+  //  check if any of the remaining numbers add up to the target
+  //  if it is return true
+  //  if we don't find a number that adds up to the target return false
 */
 
 /*
-  Add written explanation of your solution here
+  Use iteration to find the difference between each value of the array and the target
+  iterate through the rest of the array to look for a number that makes up the difference
+  return true if we find a match, and false if we don't
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 8, 1, 9, 11, 7], 10));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +47,10 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 30));
+
 }
 
 module.exports = hasTargetSum;
